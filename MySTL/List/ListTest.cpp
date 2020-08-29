c8e873ab25252c199eeca8b33299b3eb37f467d4
Zhaojun Fang <56853492+FFFcomewhere@users.noreply.github.com>
2020-08-29 10:26:20 +0800

    Update ListTest.cpp

--- MySTL/List/ListTest.cpp
+++ MySTL/List/ListTest.cpp
@@ -1,15 +1,26 @@
 #include"List.h"
-
+#include<iterator>
 int main()
 {
-    List<int> a;
-    a.push_front(10);
-    cout<<a.front()<<endl;
-    auto i = a.begin();
-    a.new_node(i,20);
-    cout<<a.front();
+    List<char> obj1{'a','b','c'};
+    obj1.show();
+    obj1.erase(obj1.begin());
+    obj1.show();
+    obj1.push_front('a');
+    obj1.show();
+    cout<<obj1.find('b')<<endl;
+    obj1.clear();
+    obj1.show();
 
 
+    List<char> obj2(3,'a');
+    obj1 = obj2;
+    obj1.show();
+    obj1.pop_front();
+    obj1.show();
+    obj1.insert(1,'c');
+    obj1.show();
+    
 
     return 0;
 }
